7ecd9d404a4034bb387792f24dd8106a959d3fce
rohithVR101 <rohithriaz11@gmail.com>
2020-05-22 22:08:57 +0530

    New edition of exisiting CricketPro repo-Premier League Edition. First commit

---
+++ src/ui/Cricket.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+/**
+ *
+ * @author Rohith Adithya
+ */
+public class Cricket {
+    public static void main(String[] args) {
+        Login start=new Login();
+        start.setVisible(true);
+    }
+    
+}
